eed62a8c2d956b36484150138af63ac63ed4f68d
Tyler Kellen <tyler@sleekcode.net>
2014-09-02 01:22:30 -0700

    cache flags

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
 
-// TODO: cache this to a file in the module matching the version number of v8
-module.exports = function (callback) {
-  exec('node --v8-options', function (err, result) {
-    callback(err, result.match(/\s\s--(\w+)/gm).map(function (match) {
-      return match.substring(4);
-    }));
+const version = process.versions.v8;
+const tmpfile = path.join(__dirname, '.flags-'+version);
+
+module.exports = function (cb) {
+  fs.exists(tmpfile, function (exists) {
+    if (exists) {
+      fs.readFile(tmpfile, function (err, flags) {
+        if (err) {
+          cb(err);
+        }
+        cb(null, JSON.parse(flags));
+      });
+    } else {
+      exec('node --v8-options', function (err, result) {
+        var flags = result.match(/\s\s--(\w+)/gm).map(function (match) {
+          return match.substring(4);
+        });
+        fs.writeFile(tmpfile, JSON.stringify(flags), { encoding:'utf8' },
+          function (writeErr) {
+            if (writeErr) {
+              cb(writeErr);
+            } else {
+              cb(null, flags);
+            }
+          }
+        );
+      });
+    }
   });
 };
