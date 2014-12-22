ae61b6d3b159988251bcae071ae5bde1e623eef0
Tyler Kellen <tyler@sleekcode.net>
2014-12-22 10:46:50 -0500

    exclude --help flag

--- fetch.js
+++ fetch.js
@@ -5,6 +5,7 @@ const exec = require('child_process').exec;
 const nodePath = process.execPath;
 const version = process.versions.v8;
 const tmpfile = path.join(__dirname, 'cache', version+'.flags.json');
+const exclusions = ['--help'];
 
 if (!fs.existsSync(tmpfile)) {
   exec('"'+nodePath+'" --v8-options', function (execErr, result) {
@@ -14,8 +15,13 @@ if (!fs.existsSync(tmpfile)) {
     } else {
       flags = result.match(/\s\s--(\w+)/gm).map(function (match) {
         return match.substring(2);
+      }).filter(function (name) {
+        return exclusions.indexOf(name) === -1;
       });
-      fs.writeFile(tmpfile, JSON.stringify(flags), { encoding:'utf8' },
+      fs.writeFile(
+        tmpfile,
+        JSON.stringify(flags),
+        { encoding:'utf8' },
         function (writeErr) {
           if (writeErr) {
             throw new Error(writeErr);
