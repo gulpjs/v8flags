eb2f3db57dc797877a00bf915b453f09bc37711a
Tyler Kellen <tyler@sleekcode.net>
2015-01-15 17:02:01 -0500

    Update: Remove fs.exists & rely on try/catch

--- index.js
+++ index.js
@@ -7,34 +7,27 @@ const tmpfile = path.join(os.tmpdir(), process.versions.v8+'.flags.json');
 const exclusions = ['--help'];
 
 module.exports = function (cb) {
-
-  fs.exists(tmpfile, function (exists) {
-    try {
-      cb(null, require(tmpfile))
-    } catch (e) {
-      execFile(process.execPath, ['--v8-options'], function (execErr, result) {
-        var flags;
-        if (execErr) {
-          return cb(execErr)
-        }
-        flags = result.match(/\s\s--(\w+)/gm).map(function (match) {
-          return match.substring(2);
-        }).filter(function (name) {
-          return exclusions.indexOf(name) === -1;
-        });
-        fs.writeFile(
-            tmpfile,
-            JSON.stringify(flags),
-          { encoding:'utf8' },
-          function (writeErr) {
-            if (writeErr) {
-              return cb(writeErr);
-            }
-            cb(null, flags);
-          }
-        );
+  try {
+    cb(null, require(tmpfile));
+  } catch (e) {
+    execFile(process.execPath, ['--v8-options'], function (execErr, result) {
+      var flags;
+      if (execErr) {
+        return cb(execErr);
+      }
+      flags = result.match(/\s\s--(\w+)/gm).map(function (match) {
+        return match.substring(2);
+      }).filter(function (name) {
+        return exclusions.indexOf(name) === -1;
       });
-    }
-  });
-
+      fs.writeFile(tmpfile, JSON.stringify(flags), { encoding:'utf8' },
+        function (writeErr) {
+          if (writeErr) {
+            return cb(writeErr);
+          }
+          cb(null, flags);
+        }
+      );
+    });
+  }
 };
