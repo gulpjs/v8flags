fdc81c952280bcaad4f20d3979c3b29c9a95ea4e
Tyler Kellen <tyler@sleekcode.net>
2015-03-28 20:22:40 -0600

    Fix: Improve caching for non-standard environments

--- test.js
+++ test.js
@@ -4,30 +4,48 @@ const path = require('path');
 
 const expect = require('chai').expect;
 
-const v8flags = require('./');
-
-const tmpfile = path.resolve(os.tmpdir(), process.versions.v8+'.flags.json');
+const env = process.env;
 
 describe('v8flags', function () {
 
-  after(function () {
-    fs.unlinkSync(tmpfile);
+  var v8flags;
+
+  beforeEach(function () {
+    delete require.cache[require.resolve('user-home')];
   });
 
-  it('should call back with the v8 flags for the running process', function (done) {
-    // if i could meaningfully test this, this libray wouldn't exist
+  it('should cache and call back with the v8 flags for the running process', function (done) {
+    var v8flags = require('./');
+    var configfile = path.resolve(require('user-home'), v8flags.configfile);
     v8flags(function (err, flags) {
       expect(flags).to.be.a('array');
-      done();
+      expect(fs.existsSync(configfile)).to.be.true;
+      fs.unlinkSync(configfile);
+      fs.writeFileSync(configfile, JSON.stringify({cached:true}));
+      v8flags(function (cacheErr, cachedFlags) {
+        expect(cachedFlags).to.deep.equal({cached:true});
+        fs.unlinkSync(configfile);
+        done();
+      });
     });
   });
 
-  it('should cache v8 flags after first run', function (done) {
+  it('should fall back to writing to a temp dir if user home can\'t be found', function (done) {
+    delete env.HOME;
+    delete env.USERPROFILE;
+    delete env.HOMEDRIVE;
+    delete env.HOMEPATH;
+    delete env.LOGNAME;
+    delete env.USER;
+    delete env.LNAME;
+    delete env.USERNAME;
+    var v8flags = require('./');
+    var configfile = path.resolve(os.tmpdir(), v8flags.configfile);
     v8flags(function (err, flags) {
-      expect(fs.existsSync(tmpfile)).to.be.true;
+      expect(fs.existsSync(configfile)).to.be.true;
+      fs.unlinkSync(configfile);
       done();
     });
   });
-
 
 });
