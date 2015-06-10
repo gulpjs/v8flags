59e1d8c477bb9813e80b8de67a02fa89cdaddaa8
Oden <ops991923@gmail.com>
2015-06-10 14:16:52 -0700

    Breaking: Use standard directories on Unix and Windows - sorry for patch version

--- test.js
+++ test.js
@@ -7,6 +7,19 @@ const expect = require('chai').expect;
 
 const env = process.env;
 
+function getConfigPath() {
+  var v8flags = require('./');
+  var userHome = require('user-home');
+  var datapath = userHome || os.tmpdir();
+  if (process.platform === 'win32' && userHome) {
+    datapath = path.join(datapath, 'AppData', 'Local');
+  }
+  else if (userHome) {
+    datapath = path.join(datapath, '.cache');
+  }
+  return path.join(datapath, v8flags.configfile);
+}
+
 describe('v8flags', function () {
 
   afterEach(function () {
@@ -14,7 +27,7 @@ describe('v8flags', function () {
     var v8flags = require('./');
     try {
       [
-        path.resolve(require('user-home'), v8flags.configfile),
+        path.resolve(getConfigPath()),
         path.resolve(os.tmpdir(), v8flags.configfile)
       ].map(fs.unlinkSync);
     } catch (e) {}
@@ -23,7 +36,7 @@ describe('v8flags', function () {
 
   it('should cache and call back with the v8 flags for the running process', function (done) {
     var v8flags = require('./');
-    var configfile = path.resolve(require('user-home'), v8flags.configfile);
+    var configfile = path.resolve(getConfigPath());
     v8flags(function (err, flags) {
       expect(flags).to.be.a('array');
       expect(fs.existsSync(configfile)).to.be.true;
@@ -38,7 +51,7 @@ describe('v8flags', function () {
 
   it('should create config correctly when multiple concurrent calls happen and it does not exist yet', function (done) {
     var v8flags = require('./');
-    var configfile = path.resolve(require('user-home'), v8flags.configfile);
+    var configfile = path.resolve(getConfigPath());
     async.parallel([v8flags, v8flags], function (err, results) {
       v8flags(function (err, final) {
         expect(results[0]).to.deep.equal(final);
