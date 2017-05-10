2830504f616a4e72999e79101e0edf52d8611195
Selwyn <talk@selwyn.cc>
2017-05-10 17:09:07 +0200

    Rename homedir to userHome for consistency

--- config-path.js
+++ config-path.js
@@ -1,23 +1,23 @@
-const homedir = require('user-home');
 const path = require('path');
+const userHome = require('user-home');
 
 const env = process.env;
 const name = 'js-v8flags';
 
 function macos () {
-	const library = path.join(homedir, 'Library');
+	const library = path.join(userHome, 'Library');
 	return path.join(library, 'Caches', name);
 };
 
 function windows () {
-	const appData = env.LOCALAPPDATA || path.join(homedir, 'AppData', 'Local');
+	const appData = env.LOCALAPPDATA || path.join(userHome, 'AppData', 'Local');
 	return path.join(appData, name, 'Cache');
 };
 
 // https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 function linux () {
-	const username = path.basename(homedir);
-	return path.join(env.XDG_CACHE_HOME || path.join(homedir, '.cache'), name);
+	const username = path.basename(userHome);
+	return path.join(env.XDG_CACHE_HOME || path.join(userHome, '.cache'), name);
 };
 
 if (process.platform === 'darwin') {
