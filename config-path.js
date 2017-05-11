3ff22beb6795900131a8115010d4c90ff15996f9
Selwyn <talk@selwyn.cc>
2017-05-11 20:42:59 +0200

    Fix space and semicolon formatting

--- config-path.js
+++ config-path.js
@@ -7,23 +7,23 @@ const name = 'js-v8flags';
 function macos () {
 	const library = path.join(userHome, 'Library');
 	return path.join(library, 'Caches', name);
-};
+}
 
 function windows () {
 	const appData = env.LOCALAPPDATA || path.join(userHome, 'AppData', 'Local');
 	return path.join(appData, name, 'Cache');
-};
+}
 
 // https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 function linux () {
 	const username = path.basename(userHome);
 	return path.join(env.XDG_CACHE_HOME || path.join(userHome, '.cache'), name);
-};
+}
 
 if (process.platform === 'darwin') {
 	module.exports = macos();
 } else if (process.platform === 'win32') {
-	module.exports =  windows();
+	module.exports = windows();
 } else {
 	module.exports = linux();
 }
