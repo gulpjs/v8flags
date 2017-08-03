64d8fd2d17a2a8cd1dffd86d8a8ef3f0f609782f
Selwyn <talk@selwyn.cc>
2017-08-04 00:34:07 +0200

    Simplify test code using uncached requires

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ const requireUncached = require('require-uncached');
 
 const env = process.env;
 
-function eraseHome() {
+function eraseHome () {
   delete env.HOME;
   delete env.USERPROFILE;
   delete env.HOMEDRIVE;
@@ -22,14 +22,14 @@ function eraseHome() {
   delete env.LOCALAPPDATA;
 }
 
-function setTemp(dir) {
+function setTemp (dir) {
   env.TMPDIR = env.TEMP = env.TMP = dir;
 }
 
 function cleanup () {
-  var v8flags = require('./');
+  const v8flags = require('./');
 
-  var files = [
+  const files = [
     path.resolve(v8flags.configPath, v8flags.configfile),
     path.resolve(os.tmpdir(), v8flags.configfile),
   ];
@@ -39,7 +39,6 @@ function cleanup () {
     } catch (e) {}
   });
 
-  delete require.cache[require.resolve('homedir-polyfill')];
   delete process.versions.electron;
 }
 
@@ -48,8 +47,8 @@ describe('v8flags', function () {
   afterEach(cleanup);
 
   it('should cache and call back with the v8 flags for the running process', function (done) {
-    var v8flags = require('./');
-    var configfile = path.resolve(v8flags.configPath, v8flags.configfile);
+    const v8flags = require('./');
+    const configfile = path.resolve(v8flags.configPath, v8flags.configfile);
     v8flags(function (err, flags) {
       expect(flags).to.be.a('array');
       expect(fs.existsSync(configfile)).to.be.true;
@@ -63,8 +62,8 @@ describe('v8flags', function () {
   });
 
   it('should not append the file when multiple calls happen concurrently and the config file does not yet exist', function (done) {
-    var v8flags = require('./');
-    var configfile = path.resolve(v8flags.configPath, v8flags.configfile);
+    const v8flags = require('./');
+    const configfile = path.resolve(v8flags.configPath, v8flags.configfile);
     async.parallel([v8flags, v8flags, v8flags], function (err, result) {
       v8flags(function (err2, res) {
         done();
@@ -93,12 +92,9 @@ describe('v8flags', function () {
   it('should fall back to writing to a temp dir if user home is unwriteable', function (done) {
     eraseHome();
     env.HOME = path.join(__dirname, 'does-not-exist');
-    // Clear require cached modules so the modified environment variable HOME is used
-    delete require.cache[require.resolve('./')];
-    delete require.cache[require.resolve('./config-path.js')];
-    var v8flags = require('./');
+    const v8flags = requireUncached('./');
     v8flags.configPath = env.HOME;
-    var configfile = path.resolve(os.tmpdir(), v8flags.configfile);
+    const configfile = path.resolve(os.tmpdir(), v8flags.configfile);
     v8flags(function (err, flags) {
       expect(fs.existsSync(configfile)).to.be.true;
       done();
@@ -108,7 +104,7 @@ describe('v8flags', function () {
   it('should return flags even if an error is thrown', function (done) {
     eraseHome();
     setTemp('/nope');
-    var v8flags = require('./');
+    const v8flags = requireUncached('./');
     v8flags(function (err, flags) {
       setTemp('/tmp');
       expect(err).to.not.be.null;
@@ -119,7 +115,7 @@ describe('v8flags', function () {
 
   it('should back with an empty array if the runtime is electron', function (done) {
     process.versions.electron = 'set';
-    var v8flags = require('./');
+    const v8flags = require('./');
     v8flags(function (err, flags) {
       expect(flags).to.have.length(0);
       expect(flags).to.be.an('array');
@@ -130,9 +126,7 @@ describe('v8flags', function () {
   it('should handle usernames which are invalid file paths', function(done) {
     eraseHome();
     env.USER = 'invalid/user\\name';
-    delete require.cache[require.resolve('./')];
-    var v8flags = require('./');
-    console.log(v8flags.configfile);
+    const v8flags = requireUncached('./');
     v8flags(function (err, flags) {
       expect(err).to.be.null;
       done();
@@ -141,9 +135,7 @@ describe('v8flags', function () {
 
   it('should handle undefined usernames', function(done) {
     eraseHome();
-    delete require.cache[require.resolve('./')];
-    var v8flags = require('./');
-    console.log(v8flags.configfile);
+    const v8flags = requireUncached('./');
     v8flags(function (err, flags) {
       expect(err).to.be.null;
       done();
@@ -154,15 +146,14 @@ describe('v8flags', function () {
 describe('config-path', function () {
   const moduleName = 'js-v8flags';
 
-  beforeEach(function() {
+  before(function () {
     env.HOME = 'somehome';
-    cleanup();
   });
-  afterEach(cleanup);
 
-  it('should return default linux path in other environments', function(done) {
-    delete require.cache[require.resolve('./config-path.js')];
-    const configPath = require('./config-path.js')('other');
+  after(cleanup);
+
+  it('should return default linux path in other environments', function (done) {
+    const configPath = requireUncached('./config-path.js')('other');
 
     expect(configPath).to.equal(
       path.join(env.HOME, '.cache', moduleName)
@@ -170,9 +161,8 @@ describe('config-path', function () {
     done();
   });
 
-  it('should return default macos path in darwin environment', function(done) {
-    delete require.cache[require.resolve('./config-path.js')];
-    const configPath = require('./config-path.js')('darwin');
+  it('should return default macos path in darwin environment', function (done) {
+    const configPath = requireUncached('./config-path.js')('darwin');
 
     expect(configPath).to.equal(
       path.join(env.HOME, 'Library', 'Caches', moduleName)
@@ -180,9 +170,8 @@ describe('config-path', function () {
     done();
   });
 
-  it('should return default windows path in win32 environment', function(done) {
-    delete require.cache[require.resolve('./config-path.js')];
-    const configPath = require('./config-path.js')('win32');
+  it('should return default windows path in win32 environment', function (done) {
+    const configPath = requireUncached('./config-path.js')('win32');
 
     expect(configPath).to.equal(
       path.join(env.HOME, 'AppData', 'Local', moduleName, 'Cache')
