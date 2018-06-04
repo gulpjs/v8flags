24622a1a528f0b7dd115e66f769d1ce0c88291c9
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-06-04 15:31:34 +0200

    Fix: Add version to the cache file name (#50)

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const path = require('path');
 const crypto = require('crypto');
 const execFile = require('child_process').execFile;
 const configPath = require('./config-path.js')(process.platform);
+const version = require("./package.json").version;
 const env = process.env;
 const user = env.LOGNAME || env.USER || env.LNAME || env.USERNAME || '';
 const exclusions = ['--help'];
 
-const configfile = '.v8flags.'+process.versions.v8+'.'+crypto.createHash('md5').update(user).digest('hex')+'.json';
+// This number must be incremented whenever the generated cache file changes.
+const CACHE_VERSION = 1;
+
+const configfile = '.v8flags-'+CACHE_VERSION+'-'+process.versions.v8+'.'+crypto.createHash('md5').update(user).digest('hex')+'.json';
 
 const failureMessage = [
   'Unable to cache a config file for v8flags to your home directory',
